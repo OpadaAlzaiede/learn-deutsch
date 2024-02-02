6f5f6bbb859996165581304414a9aa2ed5d8d8d6
obada <opadaalziede@gmail.com>
2024-02-02 16:38:05 +0300

    setup the project

---
+++ database/migrations/2024_01_30_163130_create_words_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('word');
+            $table->string('ar_translation')->comment('the arabic translation of the word.');
+            $table->string('en_translation')->comment('the english translation of the word.');
+            $table->foreignId('language_level_id')->constrained('language_levels', 'id');
+            $table->foreignId('type_id')->constrained('types', 'id');
+            $table->unsignedBigInteger('user_id')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('words');
+    }
+};
---
+++ resources/js/app.js
@@ -0,0 +1,21 @@
+require('./bootstrap')
+
+import { createApp, h } from 'vue';
+import { createInertiaApp } from '@inertiajs/vue3';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
+
+const appName = 'Learn Deutsch';
+
+createInertiaApp({
+    title: (title) => `${title} - ${appName}`,
+    resolve: (name) => require(`./Pages/${name}.vue`),
+    setup({ el, App, props, plugin }) {
+        return createApp({ render: () => h(App, props) })
+            .use(plugin)
+            .use(ZiggyVue)
+            .mount(el);
+    },
+    progress: {
+        color: '#4B5563',
+    },
+});

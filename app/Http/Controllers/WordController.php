a6193962e2fa2e88574811a7e4cd722bdb616eb1
obada <opadaalziede@gmail.com>
2024-02-11 01:23:50 +0300

    move word logic to word service

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -8,6 +8,7 @@ use App\Models\Type;
 use App\Models\Word;
 use App\Services\LanguageLevelService;
 use App\Services\TypeService;
+use App\Services\WordService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Cache;
 use Inertia\Inertia;
@@ -17,11 +18,13 @@ class WordController extends Controller
 {
     protected $languageLevelService;
     protected $typeService;
+    protected $wordService;
 
-    public function __construct(LanguageLevelService $languageLevelService, TypeService $typeService)
+    public function __construct(LanguageLevelService $languageLevelService, TypeService $typeService, WordService $wordService)
     {
         $this->languageLevelService = $languageLevelService;
         $this->typeService = $typeService;
+        $this->wordService = $wordService;
     }
 
     /**
@@ -33,21 +36,7 @@ class WordController extends Controller
         $types = $this->typeService->index();
 
         return Inertia::render('Words/Index', [
-            'words' => Word::query()
-                        ->levels($request->only('language_levels'))
-                        ->types($request->only('types'))
-                        ->orderBy('language_level_id')
-                        ->paginate(10)
-                        ->withQueryString()
-                        ->through(fn ($word) => [
-                            'id' => $word->id,
-                            'word' => $word->word,
-                            'ar_translation' => $word->ar_translation,
-                            'en_translation' => $word->en_translation,
-                            'type' => $word->type?->type,
-                            'language_level' => $word->languageLevel?->level,
-                            'user' => $word->user?->name
-                        ]),
+            'words' => $this->wordService->index($request->only('language_levels'), $request->only('types')),
             'types' => $types,
             'language_levels' => $language_levels
         ]);
@@ -69,43 +58,11 @@ class WordController extends Controller
      */
     public function store(StoreWordRequest $request)
     {
-        Word::create($request->validated());
+        $this->wordService->store($request->validated());
 
         session()->flash('message', 'created successfully');
         session()->flash('success', true);
 
         return to_route('words.index');
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
     }
 }
